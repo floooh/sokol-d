c4260f0bfb8836ed29fc8002e276a000a019b397
kassane <6756180+kassane@users.noreply.github.com>
2026-06-29 09:36:00 +0000

    chore(deps): update build.d dependency versions

--- build.d
+++ build.d
@@ -12,9 +12,9 @@ module build;
 import std;
 
 // Dependency versions
-enum emsdk_version = "5.0.7";
-enum imgui_version = "1.92.7";
-enum nuklear_version = "v4.13.3";
+enum emsdk_version = "6.0.1";
+enum imgui_version = "1.92.8";
+enum nuklear_version = "4.13.3";
 
 void main(string[] args) @safe
 {
